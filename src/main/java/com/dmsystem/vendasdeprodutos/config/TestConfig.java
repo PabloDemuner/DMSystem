680a1c15477c17fb4ea52f9eacfd58cd9a0a28cd
Pablo <pablodiegodemuner@gmail.com>
2021-03-22 23:49:58 -0400

    Configurado a associação das tabelas

--- src/main/java/com/dmsystem/vendasdeprodutos/config/TestConfig.java
+++ src/main/java/com/dmsystem/vendasdeprodutos/config/TestConfig.java
@@ -37,6 +37,8 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//TESTES DO JPA COM H2
+		
 		Category cat1 = new Category(null, "Electronics"); 
 		Category cat2 = new Category(null, "Books"); 
 		Category cat3 = new Category(null, "Computers"); 
@@ -49,6 +51,16 @@ public class TestConfig implements CommandLineRunner{
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 
 		User u1 = new User(null, "Marcia Demuner", "marcia@gmail.com", "98888888888", "123456");
 
