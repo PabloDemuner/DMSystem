6e841757cc7bb403e210daa7a13c973d179ede7f
Pablo <pablodiegodemuner@gmail.com>
2021-03-29 21:31:46 -0400

    Inserção de Usuário criado

--- src/main/java/com/dmsystem/vendasdeprodutos/resources/UserResource.java
+++ src/main/java/com/dmsystem/vendasdeprodutos/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.dmsystem.vendasdeprodutos.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dmsystem.vendasdeprodutos.entities.User;
 import com.dmsystem.vendasdeprodutos.services.UserService;
@@ -32,5 +36,18 @@ public class UserResource {
 	public ResponseEntity<User>findById(@PathVariable Long id) {
 		User obj = service.finById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		/*
+		 * return ResponseEntity.ok().body(obj);
+		 */
+		
+		//Converção que gera endereço URI Created 201
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
