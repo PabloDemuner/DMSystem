0a8e2293deb049d44d744f241f1ef906923e5737
Pablo <pablodiegodemuner@gmail.com>
2021-03-29 23:20:21 -0400

    Adicionado o tratamento de excessões para erros em finById, update e delete

--- src/main/java/com/dmsystem/vendasdeprodutos/services/UserService.java
+++ src/main/java/com/dmsystem/vendasdeprodutos/services/UserService.java
@@ -3,46 +3,63 @@ package com.dmsystem.vendasdeprodutos.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.dmsystem.vendasdeprodutos.entities.User;
 import com.dmsystem.vendasdeprodutos.repositories.UserRepository;
+import com.dmsystem.vendasdeprodutos.services.exceptions.DatabaseException;
+import com.dmsystem.vendasdeprodutos.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User finById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+			} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+			} catch (EntityNotFoundException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
-		
+
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
